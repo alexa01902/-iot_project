0ae36ad722c7a5a93154c1b691cd4f18a02cba14
alexa01902 <alexandrapapadopoulos1@gmail.com>
2024-06-30 20:19:23 +0200

    Update aio_configuration.py

--- bedroom_monitor/lib/aio_configuration.py
+++ bedroom_monitor/lib/aio_configuration.py
@@ -4,10 +4,9 @@ import ubinascii
 # Adafruit IO (AIO) configuration
 AIO_SERVER = "io.adafruit.com"
 AIO_PORT = 1883
-AIO_USER = "alexa222"
-AIO_KEY =  "aio_FCqI29UFHAK5wngpQgcmJ5YZEBm6"
+AIO_USER = "USERNAME"
+AIO_KEY =  "MY_AIO_KEY"
 AIO_CLIENT_ID = ubinascii.hexlify(machine.unique_id())  # Can be anything
-AIO_LIGHTS_FEED = "alexa222/feeds/lights"
 AIO_TEMP_FEED = "alexa222/feeds/temperature"
 AIO_HUMIDITY_FEED = "alexa222/feeds/humidity"
 AIO_DARKNESS_FEED = "alexa222/feeds/light-intensity"
